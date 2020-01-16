42e8957b24391525822a12ec6061008d90a2c277
Igor Martins <iguinhoigor4@gmail.com>
2020-01-16 15:26:24 -0300

    front-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -7,15 +8,14 @@ const app = express();
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-z7e3x.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
 });
- 
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Alssooo'});
+    return response.json({ message: 'Alooo'});
 });
 
 app.listen(3333);
